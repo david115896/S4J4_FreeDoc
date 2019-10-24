e9a461aec052b5f995fa083fc894a52146e07b88
david115896 <dtaccori@gmail.com>
2019-10-24 11:08:35 +0200

    finish

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,18 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-require 'faker'
 
-100.times do |index|
-	doc = Doctor.create!(first_name: "Dr. #{Faker::Name.first_name}", last_name: Faker::Name.last_name, specialty: Faker::Doctor.specialty, zip_code: Faker::Address.zip)
+
+require 'faker'
+specialty= ["Addiction psychiatrist","Adolescent medicine specialist","Allergist (immunologist)","Anesthesiologist","Cardiac electrophysiologist","Cardiologist","Cardiovascular surgeon","Colon and rectal surgeon","Critical care medicine specialist","Dermatologist","Developmental pediatrician","Emergency medicine specialist","Endocrinologist","Family medicine physician","Forensic pathologist","Gastroenterologist","Geriatric medicine specialist","Gynecologist","Gynecologic oncologist","Hand surgeon","Hematologist"]
+
+1.upto(100).each do |index|
+	spe_rand = rand(0..20)
+	doc = Doctor.create!(first_name: "Dr. #{Faker::Name.first_name}", last_name: Faker::Name.last_name, specialty: specialty[spe_rand], zip_code: Faker::Address.zip)
 	pat = Patient.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
 	
-	doc_rand = rand(1..index))
-	pat_rand = rand (1..index)
+	doc_rand = rand(1..index)
+	pat_rand = rand(1..index)
 	app = Appointment.create(doctor: Doctor.find(doc_rand), patient: Patient.find(pat_rand))
 end
 
